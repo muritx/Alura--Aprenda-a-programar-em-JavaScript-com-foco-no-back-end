f6b42e0773b4e75011ff2cd9797cde5df013d0ca
Murilo Teixeira <murilosantosx@hotmail.com>
2023-05-01 15:30:42 -0300

    first commit

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+//Boolean
+//True -> Verdadeiro
+//False -> False
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true; //Constantes não se alteram o valor no futuro
+
+//console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 == texto2)
